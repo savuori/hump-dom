6a1ba1b2f00dad2dda5673ad26eaa6891c0eb828
Sampo Vuori <sampo@kepit.fi>
2016-09-03 02:43:36 +0300

    try handling properties in a well-typed manner

--- main_entry.ml
+++ main_entry.ml
@@ -9,22 +9,52 @@ type _ vNodeContent =
   | Text : string vNodeContent
   | V: (vNode array) vNodeContent;;
 
+
+type styleProperty =
+  | BackgroundColor of string;;
+
+type htmlProperty =
+  | Style of styleProperty;;
+
 external sdClass : snabbDomModule = "snabbdom/modules/class" [@@bs.module];;
 external sdStyle : snabbDomModule = "snabbdom/modules/style" [@@bs.module];;
 
-external dom : document = "document" [@@bs.val];;
-external getElementById : document -> string -> vNode = "" [@@bs.send];;
+external dom : document = "document" [@@bs.val]
+external getElementById : document -> string -> vNode = "" [@@bs.send]
 
-external init : snabbDomModule array -> (vNode -> vNode -> unit [@bs]) = "init" [@@bs.module "snabbdom"];;
+external init : snabbDomModule array -> (vNode -> vNode -> unit [@bs]) = "init" [@@bs.module "snabbdom"]
 
 external h : string -> < .. > Js.t -> 'a -> ('a vNodeContent [@bs.ignore]) -> vNode = "snabbdom/h" [@@bs.module]
 
-let h_ a b c = h a [%bs.obj {a = "5"}] b c
+external makeHtmlProperties : ?style: < .. > Js.t -> ?on: < .. > Js.t -> unit -> < .. > Js.t = "" [@@bs.obj]
+
+external makeStyleProperties : ?backgroundColor: string -> unit -> < .. > Js.t = "" [@@bs.obj]
+
+let handleStyleProperty prop =
+  match prop with
+  | BackgroundColor color -> makeStyleProperties ~backgroundColor: color
+
+let handleHtmlProperty prop =
+  match prop with
+  | Style style -> makeHtmlProperties ~style: ((handleStyleProperty style) ())
+
+(* let handleHtmlProperties props =
+  List.fold_right (handleHtmlProperty props [%bs.obj {a = "5"}] *)
+
+let h_ a b c = h a [%bs.obj {a = "5"}] b c;;
+
+let html tag props type' children = h tag ((handleHtmlProperty props) ()) children type';;
 
 let patch = init [| sdClass; sdStyle |];;
 
+(*
 let vnode = h_ "div" [|
     h "div" [%bs.obj {style = {backgroundColor = "red"}}] "Hello World!" Text
+  |] V
+  *)
+
+let vnode = h_ "div" [|
+    html "div" (Style (BackgroundColor "blue")) Text "Hello World!"
   |] V
 
 let container = getElementById dom "container";;
