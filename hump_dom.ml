2593e6e06f539406eb2050ba73292d6b3854e1ab
Sampo Vuori <sampo@kepit.fi>
2016-09-07 20:19:06 +0300

    start refactoring general parts into a module

---
+++ hump_dom.ml
@@ -0,0 +1,80 @@
+type snabbDomModule;;
+
+type vNode;;
+type document;;
+type window;;
+type timer;;
+
+type propObj;;
+
+type _ vNodeContent =
+  | Text : string vNodeContent
+  | V: (vNode array) vNodeContent;;
+
+type styleProperty =
+  | BackgroundColor of string
+  | Color of string;;
+
+type 'a htmlProperty =
+  | Style of (styleProperty list)
+  | OnClick of 'a;;
+
+external sdClass : snabbDomModule = "snabbdom/modules/class" [@@bs.module];;
+external sdStyle : snabbDomModule = "snabbdom/modules/style" [@@bs.module];;
+external sdEventListeners : snabbDomModule = "snabbdom/modules/eventlisteners" [@@bs.module];;
+
+external dom : document = "document" [@@bs.val];;
+external win : window = "window" [@@bs.val];;
+
+external getElementById : document -> string -> vNode = "" [@@bs.send]
+external setTimeout : window -> (unit -> unit [@bs]) -> float -> timer = "" [@@bs.send]
+
+external init : snabbDomModule array -> (vNode -> vNode -> vNode [@bs]) = "init" [@@bs.module "snabbdom"]
+
+external snabbh : string -> < .. > Js.t -> ('a vNodeContent [@bs.ignore]) -> 'a -> vNode = "snabbdom/h" [@@bs.module]
+
+external makeObj : unit -> < .. > Js.t = "" [@@bs.obj]
+
+let messages = ref [];;
+
+let styleHandler prop obj =
+  match prop with
+  | BackgroundColor color -> obj##backgroundColor #= color;
+                             obj
+  | Color color -> obj##color #= color;
+                   obj
+
+let handleProperties handler props =
+  List.fold_right handler props (makeObj ());;
+
+let queueAction action =
+  messages := action :: !messages;;
+
+let htmlHandler prop obj =
+  match prop with
+  | Style styles -> obj##style #= (handleProperties styleHandler styles);
+                    obj
+  | OnClick action -> obj##on #= [%bs.obj {click = fun _ -> queueAction action}];
+                      obj
+
+let h_ a b c = snabbh a (makeObj ()) b c;;
+
+let h tag props type' children = snabbh tag (handleProperties htmlHandler props) type' children;;
+
+let patch = init [| sdClass; sdStyle; sdEventListeners |];;
+
+
+let startApp containerId viewFun updateFun startModel =
+  let container = getElementById dom containerId in
+  let initVTree = patch container (viewFun startModel) [@bs] in
+  let rec messageLoop model tree =
+    let (newModel, newTree) = match !messages with
+                              | [] -> (model, tree)
+                              | actionList -> let newM = List.fold_right updateFun actionList model in
+                                              let newT = patch tree (viewFun newM) [@bs] in
+                                                messages := [];
+                                                (newM, newT)
+    in
+    setTimeout win (fun () -> messageLoop newModel newTree; [@bs]) 25.0; ();
+  in
+  messageLoop startModel initVTree;;
--- main_entry.ml
+++ main_entry.ml
@@ -1,76 +1,12 @@
-type snabbDomModule;;
-
-type vNode;;
-type document;;
-type window;;
-type timer;;
-
-type propObj;;
-
-type _ vNodeContent =
-  | Text : string vNodeContent
-  | V: (vNode array) vNodeContent;;
+open Hump_dom
 
 type action =
  | Juttu;;
 
-type styleProperty =
-  | BackgroundColor of string
-  | Color of string;;
-
-type 'a htmlProperty =
-  | Style of (styleProperty list)
-  | OnClick of 'a;;
-
 type model = { thing: string};;
 
-
-external sdClass : snabbDomModule = "snabbdom/modules/class" [@@bs.module];;
-external sdStyle : snabbDomModule = "snabbdom/modules/style" [@@bs.module];;
-external sdEventListeners : snabbDomModule = "snabbdom/modules/eventlisteners" [@@bs.module];;
-
-external dom : document = "document" [@@bs.val];;
-external win : window = "window" [@@bs.val];;
-
-external getElementById : document -> string -> vNode = "" [@@bs.send]
-external setTimeout : window -> (unit -> unit [@bs]) -> float -> timer = "" [@@bs.send]
-
-external init : snabbDomModule array -> (vNode -> vNode -> vNode [@bs]) = "init" [@@bs.module "snabbdom"]
-
-external snabbh : string -> < .. > Js.t -> ('a vNodeContent [@bs.ignore]) -> 'a -> vNode = "snabbdom/h" [@@bs.module]
-
-external makeObj : unit -> < .. > Js.t = "" [@@bs.obj]
-
-let messages = ref [];;
-
-let styleHandler prop obj =
-  match prop with
-  | BackgroundColor color -> obj##backgroundColor #= color;
-                             obj
-  | Color color -> obj##color #= color;
-                   obj
-
-let handleProperties handler props =
-  List.fold_right handler props (makeObj ());;
-
-let queueAction action =
-  messages := action :: !messages;
-  (Js.log (messages));;
-
-let htmlHandler prop obj =
-  match prop with
-  | Style styles -> obj##style #= (handleProperties styleHandler styles);
-                    obj
-  | OnClick action -> obj##on #= [%bs.obj {click = fun _ -> queueAction action}];
-                      obj
-
-let h_ a b c = snabbh a (makeObj ()) b c;;
-
-let h tag props type' children = snabbh tag (handleProperties htmlHandler props) type' children;;
-
-let patch = init [| sdClass; sdStyle; sdEventListeners |];;
-
 let initModel = { thing = "Hello World!" };;
+
 
 let view model = h_ "div" V [|
     h "div" [ Style [BackgroundColor "blue"; Color "red"]
@@ -82,20 +18,4 @@ let update action model =
   match action with
   | Juttu -> { model with thing = "Hello again!" };;
 
-let container = getElementById dom "container";;
-
-let patcher model = patch container (view model) [@bs];;
-
-let initVTree = patch container (view initModel) [@bs];;
-
-let rec messageLoop model tree =
-  let (newModel, newTree) = match !messages with
-                            | [] -> (model, tree)
-                            | actionList -> let newM = List.fold_right update actionList model in
-                                            let newT = patch tree (view newM) [@bs] in
-                                              messages := [];
-                                              (newM, newT)
-  in
-  setTimeout win (fun () -> messageLoop newModel newTree; [@bs]) 25.0; ();;
-
-messageLoop initModel initVTree;;
+startApp "container" view update initModel
