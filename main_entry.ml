8af77e774c4b765706ecae86e0e221877af13797
Sampo Vuori <sampo@kepit.fi>
2016-09-07 19:01:41 +0300

    begin work on handling messages

--- main_entry.ml
+++ main_entry.ml
@@ -2,6 +2,8 @@ type snabbDomModule;;
 
 type vNode;;
 type document;;
+type window;;
+type timer;;
 
 type propObj;;
 
@@ -10,8 +12,7 @@ type _ vNodeContent =
   | V: (vNode array) vNodeContent;;
 
 type action =
- | Juttu
- | TokaJuttu;;
+ | Juttu;;
 
 type styleProperty =
   | BackgroundColor of string
@@ -21,19 +22,26 @@ type 'a htmlProperty =
   | Style of (styleProperty list)
   | OnClick of 'a;;
 
+type model = { thing: string};;
+
 
 external sdClass : snabbDomModule = "snabbdom/modules/class" [@@bs.module];;
 external sdStyle : snabbDomModule = "snabbdom/modules/style" [@@bs.module];;
 external sdEventListeners : snabbDomModule = "snabbdom/modules/eventlisteners" [@@bs.module];;
 
-external dom : document = "document" [@@bs.val]
-external getElementById : document -> string -> vNode = "" [@@bs.send]
+external dom : document = "document" [@@bs.val];;
+external win : window = "window" [@@bs.val];;
 
-external init : snabbDomModule array -> (vNode -> vNode -> unit [@bs]) = "init" [@@bs.module "snabbdom"]
+external getElementById : document -> string -> vNode = "" [@@bs.send]
+external setTimeout : window -> (unit -> unit [@bs]) -> float -> timer = "" [@@bs.send]
+
+external init : snabbDomModule array -> (vNode -> vNode -> vNode [@bs]) = "init" [@@bs.module "snabbdom"]
 
 external snabbh : string -> < .. > Js.t -> ('a vNodeContent [@bs.ignore]) -> 'a -> vNode = "snabbdom/h" [@@bs.module]
 
 external makeObj : unit -> < .. > Js.t = "" [@@bs.obj]
+
+let messages = ref [];;
 
 let styleHandler prop obj =
   match prop with
@@ -45,9 +53,9 @@ let styleHandler prop obj =
 let handleProperties handler props =
   List.fold_right handler props (makeObj ());;
 
-
 let queueAction action =
-  (Js.log (action));;
+  messages := action :: !messages;
+  (Js.log (messages));;
 
 let htmlHandler prop obj =
   match prop with
@@ -62,12 +70,34 @@ let h tag props type' children = snabbh tag (handleProperties htmlHandler props)
 
 let patch = init [| sdClass; sdStyle; sdEventListeners |];;
 
-let vnode = h_ "div" V [|
+let initModel = { thing = "Hello World!" };;
+
+let view model = h_ "div" V [|
     h "div" [ Style [BackgroundColor "blue"; Color "red"]
             ; OnClick Juttu ]
-            Text "Hello World!"
+            Text model.thing
   |]
+
+let update action model =
+  match action with
+  | Juttu -> { model with thing = "Hello again!" };;
 
 let container = getElementById dom "container";;
 
-patch container vnode [@bs]
+let patcher model = patch container (view model) [@bs];;
+
+let initVTree = patch container (view initModel) [@bs];;
+
+let testi action model = patch container (view (update action model)) [@bs];;
+
+let rec messageLoop model tree =
+  let (newModel, newTree) = match !messages with
+                            | [] -> Js.log "no messages"; (model, tree)
+                            | action :: t -> let newM = update action model in
+                                             let newT = patch tree (view newM) [@bs] in
+                                               messages := t;
+                                               (newM, newT)
+  in
+  setTimeout win (fun () -> messageLoop newModel newTree; [@bs]) 100.0; ();;
+
+messageLoop initModel initVTree;;
